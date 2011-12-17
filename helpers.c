b0752105d451b303921e3077553c238dc8cd10cc
spukst3r <spukst3r@gmail.com>
2011-12-17 14:43:51 +0400

    die func is a bit more usable now

--- helpers.c
+++ helpers.c
@@ -2,10 +2,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 void die(const char *message)
 {
-	fprintf(stderr, "%s\n", message);
+	if (errno != 0)
+		perror(message);
+	else
+		fprintf(stderr, "%s\n", message);
 	exit(-1);
 }
 
